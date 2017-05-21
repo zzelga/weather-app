10e2217dce09a360f240cdbb1b33e40cd5238067
Zbigniew Zelga <zbigniew.zelga@gmail.com>
2017-05-21 19:51:03 -0400

    Log formatted address, lat/lng coordinates from response

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=grindhouse%20coffee%20philadelphia',
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, null, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });

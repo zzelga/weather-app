ef1c9fd0301f08bf469481af288e0f1edb2e3c6b
Zbigniew Zelga <zbigniew.zelga@gmail.com>
2017-05-21 18:58:33 -0400

    Setup hard-coded request to Google Geolocation API

---
+++ app.js
@@ -0,0 +1,8 @@
+const request = require('request');
+
+request({
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=grindhouse%20coffee%20philadelphia',
+  json: true
+}, (error, response, body) => {
+  console.log(body);
+});

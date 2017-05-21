e425a856bef8fae7d1a125eddbd47e53d62a7267
Zbigniew Zelga <zbigniew.zelga@gmail.com>
2017-05-21 19:01:31 -0400

    Pretty print API response body with JSON.stringify

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=grindhouse%20coffee%20philadelphia',
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(JSON.stringify(body, null, 2));
 });

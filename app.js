cbd4a0b04bf686bd63a5d91f35bb2048c3f85f6d
Zbigniew Zelga <zbigniew.zelga@gmail.com>
2017-05-21 21:06:36 -0400

    Add callback to geocoding function

--- app.js
+++ app.js
@@ -14,4 +14,10 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-geocode.geocodeAddress(argv.address);
+geocode.geocodeAddress(argv.address, (error, results) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.stringify(results, null, 2));
+  }
+});
--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-var geocodeAddress = (address) => {
+var geocodeAddress = (address, callback) => {
   var encodedAddress = encodeURIComponent(address);
 
   request({
@@ -8,13 +8,15 @@ var geocodeAddress = (address) => {
     json: true
   }, (error, response, body) => {
     if (error) {
-      console.log('Unable to connect to Google servers.');
+      callback('Unable to connect to Google servers.');
     } else if (body.status === 'ZERO_RESULTS') {
-      console.log('Unable to find that address.');
+      callback('Unable to find that address.');
     } else if (body.status === 'OK') {
-      console.log(`Address: ${body.results[0].formatted_address}`);
-      console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-      console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+      callback(null, {
+        address: body.results[0].formatted_address,
+        latitude: body.results[0].geometry.location.lat,
+        longitude: body.results[0].geometry.location.lng
+      });
     }
   });
 }

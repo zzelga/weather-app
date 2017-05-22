ebc416427ad878e592272e7a255c9120dd17e16b
Zbigniew Zelga <zbigniew.zelga@gmail.com>
2017-05-21 20:14:39 -0400

    Encode provided address for use in API request

--- app.js
+++ app.js
@@ -14,8 +14,10 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
+const encodedAddress = encodeURIComponent(argv.address);
+
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=grindhouse%20coffee%20philadelphia',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);

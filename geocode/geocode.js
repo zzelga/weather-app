027a62642b7a17ddb23c4e7c4f72e3e0d54d1055
Zbigniew Zelga <zbigniew.zelga@gmail.com>
2017-05-21 21:01:46 -0400

    Refactor geocoding functionality into separate file

---
+++ geocode/geocode.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+var geocodeAddress = (address) => {
+  var encodedAddress = encodeURIComponent(address);
+
+  request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    json: true
+  }, (error, response, body) => {
+    if (error) {
+      console.log('Unable to connect to Google servers.');
+    } else if (body.status === 'ZERO_RESULTS') {
+      console.log('Unable to find that address.');
+    } else if (body.status === 'OK') {
+      console.log(`Address: ${body.results[0].formatted_address}`);
+      console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+      console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
+  });
+}
+
+module.exports = {
+  geocodeAddress
+};

1f4a5adb7f98739dec82d07c6ed6de8809e74f63
Zbigniew Zelga <zbigniew.zelga@gmail.com>
2017-05-21 21:43:32 -0400

    Implement working request to Forecast.io API
    This currently makes use of hard-coded long/lat coordinates for
    Disney World, and a hard-coded (throwaway) API key.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const request = require('request');
 const yargs = require('yargs');
 const geocode = require('./geocode/geocode');
 
@@ -19,5 +20,20 @@ geocode.geocodeAddress(argv.address, (error, results) => {
     console.log(error);
   } else {
     console.log(JSON.stringify(results, null, 2));
+  }
+});
+
+const forecast_io_api_key = '87e8139acc3901b3e9d40669941841e1';
+const latitude = 28.385233;
+const longitude = -81.563874;
+
+request({
+  url: `https://api.darksky.net/forecast/${forecast_io_api_key}/${latitude},${longitude}`,
+  json: true
+}, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    console.log(body.currently.temperature);
+  } else {
+    console.log('Unable to fetch weather.');
   }
 });
